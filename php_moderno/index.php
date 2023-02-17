35bfca7acb29afbef7b78dcff8848805e4e567e6
Clóvis Balreira Rodrigues <62964663+clovisbalreira@users.noreply.github.com>
2023-02-17 17:57:21 -0300

    17-02-2023

--- php_moderno/index.php
+++ php_moderno/index.php
@@ -13,6 +13,10 @@
         <a href="./ex000/index.php">Comando echo, emoj</a>
         <a href="./ex001/index.php">Dados do servidor</a>
         <a href="./ex002/index.php">Funções</a>
+        <a href="./ex003/index.php">Variaveis e constantes</a>
+        <a href="./ex004/index.php">Tipos Primitivos</a>
+        <a href="./ex005/index.php">Tipos String</a>
+        <a href="./ex006/index.php">Formulario</a>
     </div>
 </body>
 </html>
