8bc0c2048f5b5369359ec329b3b2fdb907a35312
Clóvis Balreira Rodrigues <62964663+clovisbalreira@users.noreply.github.com>
2023-05-02 11:40:11 -0300

    02-03-2023

--- sites/arduino/index.php
+++ sites/arduino/index.php
@@ -4,19 +4,45 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Conexão com arduino</title>
+    <!-- <meta http-equiv="refresh" content="30"> -->
+    <title>Servidor com arduino</title>
 </head>
 <body>
     <?php
-        echo "Número Lido: ";
-        echo $_REQUEST["numero"];
-        //$conecaoArduino = Fopen('porta', 'methodo')
-        //$conecaoArduino = fopen('porta', 'w');
-        //fwrite($conecaoArduino,$_REQUEST["numero"]);//escrever
-        //fclose($conecaoArduino);
-        //aula 4559
+        $arduino = $_REQUEST["arduino"] ?? 'Não';
+        $lambada = $_REQUEST["lampada"] ?? 'Apagar';
+        $conecaoArduino = fopen('COM5', 'w'); // conexao com porta serial parametro porta , metodo
+        sleep(2); // delay
+        
+        if($lambada == 'Apagar'){
+            $acao = '0';
+            $imagemLambada = './img/lampada-apagada.jpg';
+        }else{
+            $acao = '1';
+            $imagemLambada = './img/lampada-acesa.jpg';
+        }
+        if($arduino == 'Sim'){
+            //fwrite($conecaoArduino,$acao); // onde vai escrever e o que quer escrever
+            //fclose($conecaoArduino); // fechar conexao
+        }
+        //aula 4599
+        /* 
+        criar redes externa
+        1) acessar o modem dsl 192.168.1.1
+        2) redirecionar uma rota para o endereço ip da pagina que você tem o arduino - nat virtual server
+        3) acessar meuip.com.br
+        */
     ?>
-    <li><a href="arduino-1.8.19-windows.exe" download="arduino-1.8.19-windows.exe" type="application/exe">Baixar o livro em PDF</a></li>
-    <li><a href="CH34x_Install_Windows_v3_4.zip" download="CH34x_Install_Windows_v3_4.zip" type="application/zip">Livro compactado em ZIP</a></li>
+    <h1>Controles de arduino</h1>
+    <form action="<?= $_SERVER['PHP_SELF']?>" method="post">
+        <h2>Possui arduino</h2>
+        <input type="submit" name="arduino" value="Sim">
+        <input type="submit" name="arduino" value="Não">
+    </form>
+    <form action="<?= $_SERVER['PHP_SELF']?>" method="get">
+        <img id="lambada" src="<?=$imagemLambada ?>" alt="Lambada">
+        <input type="submit" name="lampada" value="Acender">
+        <input type="submit" name="lampada" value="Apagar">
+    </form>
 </body>
 </html>
